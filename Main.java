a8fe871197648e58c189728a18f9f7f2d3b69e91
Pricah <pricalyf@gmail.com>
2025-03-27 09:37:45 +0300

    this is the main class

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new RegistrationForm().setVisible(true);
+        });
+    }
+}
